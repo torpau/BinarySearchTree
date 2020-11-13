5c8a01457294b25c51c57cf38b6b4a6ea9897f23
Filip Peric <antonyfilip@hotmail.com>
2020-11-13 15:00:41 +0100

    final delete funtion fix

--- src/com/company/BinaryTree.java
+++ src/com/company/BinaryTree.java
@@ -4,7 +4,7 @@ public class BinaryTree {
     Node root;
     Node parent;
 
-    class Node{
+    class Node {
         Node left;
         Node right;
         int key;
@@ -19,8 +19,8 @@ public class BinaryTree {
         printTreeInOrder(root);
     }
 
-    private void printTreeInOrder(Node tree){
-        if(tree == null){
+    private void printTreeInOrder(Node tree) {
+        if (tree == null) {
             return;
         }
         printTreeInOrder(tree.left);
@@ -29,12 +29,12 @@ public class BinaryTree {
     }
 
     // -----------
-    public void printTreeInReverseOrder(){
+    public void printTreeInReverseOrder() {
         printTreeInReverseOrder(root);
     }
 
     private void printTreeInReverseOrder(Node tree) {
-        if(tree == null){
+        if (tree == null) {
             return;
         }
         printTreeInReverseOrder(tree.right);
@@ -43,12 +43,12 @@ public class BinaryTree {
     }
 
     // -----------
-    public void printTreePostOrder(){
+    public void printTreePostOrder() {
         printTreePostOrder(root);
     }
 
     private void printTreePostOrder(Node tree) {
-        if(tree == null){
+        if (tree == null) {
             return;
         }
         printTreePostOrder(tree.left);
@@ -57,36 +57,117 @@ public class BinaryTree {
     }
 
     // -----------
-    public void printTreePreOrder(){
+    public void printTreePreOrder() {
         printTreePreOrder(root);
     }
 
     private void printTreePreOrder(Node tree) {
-        if(tree == null){
+        if (tree == null) {
             return;
         }
         System.out.println(tree.key);
         printTreePreOrder(tree.left);
         printTreePreOrder(tree.right);
     }
+
     // -----------
-    public void insert(int key){
+    public void insert(int key) {
         root = insertRec(root, key);
     }
 
     private Node insertRec(Node tree, int key) {
-        if(tree == null){
+        if (tree == null) {
             tree = new Node(key);
             return tree;
         }
-        if(key < tree.key){
+        if (key < tree.key) {
             tree.left = insertRec(tree.left, key);
-        }
-        else if (key > tree.key){
+        } else if (key > tree.key) {
             tree.right = insertRec(tree.right, key);
         }
         return tree;
     }
+
+
+    public boolean remove(int key) {
+        Node currNode = root;
+        Node parentNode = root;
+        boolean isLeft = true;
+        while (currNode.key != key) {
+            parentNode = currNode;
+            if(key< currNode.key){
+                isLeft = true;
+                currNode = currNode.left;
+
+            }else{
+                isLeft=false;
+                currNode = currNode.right;
+            }
+            if(currNode == null){return false;}
+        }
+        if(currNode.left == null && currNode.right == null){
+            if(currNode == root){
+                root = null;
+            } else if(isLeft == true){
+                parentNode.left = null;
+            }else{
+                parentNode.right = null;
+            }
+
+        }
+        else if(currNode.right == null){
+            if(currNode == root){
+                root = currNode.left;
+            }else if(isLeft){
+                parentNode.left = currNode.left;
+            } else{
+                parentNode.right = currNode.left;
+            }
+        }else if(currNode.left == null){
+            if(currNode == root){
+                root = currNode.right;
+            } else if (isLeft == true){
+                parentNode.left = currNode.right;
+            } else{
+                parentNode.right = currNode.left; // enl osäker källa ska currNode.right vara currNode.left;
+            }
+        }else{
+            Node replace = getReplaceNode(currNode);
+
+            if(currNode == root){
+                root = replace;
+            }else if(isLeft){
+                parentNode.left = replace;
+            }else{
+                parentNode.right = replace;
+            }
+            replace.left = currNode.left;
+        }
+
+
+        return true;
+    }
+    public Node getReplaceNode(Node replaceNode) {
+        Node replaceParent = replaceNode;
+        Node replace = replaceNode;
+
+        Node currNode = replaceNode.right;
+
+        while (currNode != null) {
+            replaceParent = replace;
+            replace = currNode;
+            currNode = currNode.left;
+
+        }
+        if (replace != replaceNode.right) {
+            replaceParent.left = replace.right;
+            replace.right = replaceNode.right;
+        }
+
+        return replace;
+    }
+}
+
 /*
     public void deleteTreeObject(int key) {
         Node currentIndex = root;
@@ -103,13 +184,14 @@ public class BinaryTree {
     }
 
  */
-
+/*
     public void deleteObjectInTree(int key){
 
         root = deleteObjectInTree(root, key);
 
 
     }
+
     private Node deleteObjectInTree(Node tree, int key){
         if(tree == null) {
             return tree;
@@ -155,10 +237,10 @@ public class BinaryTree {
                 parent.right = tree.right;
             }
 
-             */
+
             System.out.println("p"+parent.key);
             System.out.println(tree.key);
-            return tree;
+           return tree;
 
         }
 
@@ -169,7 +251,7 @@ public class BinaryTree {
 
         return tree;
     }
-/*
+
     public static void main(String [] Args){
         BinaryTree bt = new BinaryTree();
 
@@ -190,8 +272,23 @@ public class BinaryTree {
                  print 6
                  printTreeInOrder(null)
              print 8
-         */
+
 
 
     }
+
+ */
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        BinaryTreeTwo binaryTree = new BinaryTreeTwo();
 
-
-        binaryTree.addNodeToTree("forsta ", 50);
-        binaryTree.addNodeToTree("andra ", 25);
-        binaryTree.addNodeToTree("tredje", 15);
-        binaryTree.addNodeToTree("fjarde",30);
-        binaryTree.addNodeToTree("femte", 75);
-        binaryTree.addNodeToTree("sjätte", 85);
-         */
 
         BinaryTree bT = new BinaryTree();
 
@@ -24,12 +14,14 @@ public class Main {
         bT.insert(75);
         bT.insert(85);
         bT.insert(72);
+        bT.insert(26);
+        bT.insert(27);
 
-        bT.deleteObjectInTree(75);
+        
         System.out.println("printTreeInOrder");
         bT.printTreeInOrder();
 
-/*
+
         System.out.println("printTreeInOrder");
         bT.printTreeInOrder();
 
@@ -41,6 +33,6 @@ public class Main {
 
         System.out.println("printTreePostOrder");
         bT.printTreePostOrder();
-*/
+
     }
 }
